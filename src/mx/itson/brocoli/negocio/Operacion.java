5323b31b349a658af2096821855f25e05ca297e8
arana <arana@LAPTOP-FT6SD15R>
2023-09-08 10:23:10 -0700

    Versión inicial del proyecto

---
+++ src/mx/itson/brocoli/negocio/Operacion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.brocoli.negocio;
+
+/**
+ * Contiene metodos para trabar con tipos de datos String.
+ * @author  Danett Arana
+ */
+public class Operacion {
+ 
+    /**
+     * Separa una cadena de texto a partir de la expresión guion medio.
+     * @param valor Cadena de texto que será separada a partir de la expresión.
+     * @return Array de String en donde cada posición representa una división de la cadena de texto.
+     */
+    public String[] separar(String valor){
+     String[] elementos = valor.split("-");
+         return elementos;
+              
+         
+        
+        
+    }
+    
+    
+}
